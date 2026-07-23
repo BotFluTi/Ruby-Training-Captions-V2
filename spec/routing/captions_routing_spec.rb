6d3ab49cf2405da995e18948d2c646ee758f5cbf
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2026-07-23 21:52:26 +0300

    Routes spec

---
+++ spec/routing/captions_routing_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Caption routes", type: :routing do
+  it "routes GET /captions to index" do
+    expect(get: "/captions")
+      .to route_to(controller: "captions", action: "index")
+  end
+
+  it "routes GET /captions/:id to show" do
+    expect(get: "/captions/12")
+      .to route_to(controller: "captions", action: "show", id: "12")
+  end
+
+  it "routes POST /captions to create" do
+    expect(post: "/captions")
+      .to route_to(controller: "captions", action: "create")
+  end
+
+  it "routes DELETE /captions/:id to destroy" do
+    expect(delete: "/captions/12")
+      .to route_to(controller: "captions", action: "destroy", id: "12")
+  end
+end
