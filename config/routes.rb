8d7995f74f3cad2ebcf25658cf7ab2aeefec1a64
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2026-07-24 00:48:08 +0300

    define instagram caption endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get "/captions", to: "captions#index"
+  get "/captions/instagrams", to: "instagram_captions#index"
   get "/captions/:id", to: "captions#show"
+
   post "/captions", to: "captions#create"
+  post "/captions/instagram", to: "instagram_captions#create"
+
   delete "/captions/:id", to: "captions#destroy"
 
   post "/signup", to: "users#signup"
