493d432aff84ce04a2bc79529d6fb676a04731b9
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2026-07-23 22:13:12 +0300

    caption validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_19_164000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_23_190659) do
+  create_table "captions", force: :cascade do |t|
+    t.string "caption_url"
+    t.datetime "created_at", null: false
+    t.string "text"
+    t.datetime "updated_at", null: false
+    t.string "url"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "password_digest", null: false
