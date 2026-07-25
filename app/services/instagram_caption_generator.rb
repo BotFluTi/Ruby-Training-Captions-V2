54810fe899cee25bbceabc21dce092d6c3d2488e
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2026-07-25 21:38:07 +0300

    crop instagram images

--- app/services/instagram_caption_generator.rb
+++ app/services/instagram_caption_generator.rb
@@ -6,6 +6,12 @@ class InstagramCaptionGenerator
   OUTPUT_FOLDER = Rails.root.join("images", "generated")
   INSTAGRAM_SIZE = "1080x1080"
 
+  MINIMUM_WIDTH = 320
+  MAXIMUM_WIDTH = 1080
+
+  MINIMUM_ASPECT_RATIO = 0.75
+  MAXIMUM_ASPECT_RATIO = 1.91
+
   def initialize(caption)
     @caption = caption
   end
@@ -20,6 +26,18 @@ class InstagramCaptionGenerator
     )
   end
 
+  def generate_image(file_path)
+    FileUtils.mkdir_p(OUTPUT_FOLDER)
+
+    image = MiniMagick::Image.open(file_path)
+    resize_and_crop_image(image)
+
+    output_path = caption_path
+    image.write(output_path)
+
+    output_path
+  end
+
   private
 
   attr_reader :caption
@@ -27,16 +45,43 @@ class InstagramCaptionGenerator
   def generate_background(background)
     FileUtils.mkdir_p(OUTPUT_FOLDER)
 
-    caption_path = OUTPUT_FOLDER.join(
-      "instagram_#{SecureRandom.uuid}.png"
-    ).to_s
+    output_path = caption_path
 
     MiniMagick.convert do |convert|
       convert.size INSTAGRAM_SIZE
       convert << background
-      convert << caption_path
+      convert << output_path
     end
 
-    caption_path
+    output_path
+  end
+
+  def resize_and_crop_image(image)
+    width = image.width
+    height = image.height
+    target_width = width.clamp(MINIMUM_WIDTH, MAXIMUM_WIDTH)
+    aspect_ratio = width.to_f / height
+
+    if aspect_ratio > MAXIMUM_ASPECT_RATIO
+      target_height = (target_width / MAXIMUM_ASPECT_RATIO).ceil
+      crop_image(image, target_width, target_height)
+    elsif aspect_ratio < MINIMUM_ASPECT_RATIO
+      target_height = (target_width / MINIMUM_ASPECT_RATIO).floor
+      crop_image(image, target_width, target_height)
+    elsif target_width != width
+      image.resize("#{target_width}x")
+    end
+  end
+
+  def crop_image(image, width, height)
+    image.resize("#{width}x#{height}^")
+    image.gravity("center")
+    image.extent("#{width}x#{height}")
+  end
+
+  def caption_path
+    OUTPUT_FOLDER.join(
+      "instagram_#{SecureRandom.uuid}.png"
+    ).to_s
   end
 end
--- spec/services/instagram_caption_generator_spec.rb
+++ spec/services/instagram_caption_generator_spec.rb
@@ -90,4 +90,94 @@ RSpec.describe InstagramCaptionGenerator do
       expect(result).to eq(caption_path)
     end
   end
+
+  describe "#generate_image" do
+    let(:caption) { instance_double(InstagramCaption) }
+    let(:original_path) { "images/original_123.png" }
+    let(:width) { 800 }
+    let(:height) { 800 }
+
+    let(:image) do
+      double(
+        "image",
+        width: width,
+        height: height,
+        resize: nil,
+        gravity: nil,
+        extent: nil,
+        write: nil
+      )
+    end
+
+    before do
+      allow(MiniMagick::Image)
+        .to receive(:open)
+              .with(original_path)
+              .and_return(image)
+    end
+
+    context "when the image width is below 320 pixels" do
+      let(:width) { 200 }
+      let(:height) { 200 }
+
+      it "resizes the image to 320 pixels" do
+        generator.generate_image(original_path)
+
+        expect(image).to have_received(:resize).with("320x")
+      end
+    end
+
+    context "when the image dimensions are supported" do
+      it "keeps the original dimensions" do
+        generator.generate_image(original_path)
+
+        expect(image).not_to have_received(:resize)
+        expect(image).not_to have_received(:extent)
+      end
+    end
+
+    context "when the image width exceeds 1080 pixels" do
+      let(:width) { 1500 }
+      let(:height) { 1500 }
+
+      it "resizes the image to 1080 pixels" do
+        generator.generate_image(original_path)
+
+        expect(image).to have_received(:resize).with("1080x")
+      end
+    end
+
+    context "when the image is too wide" do
+      let(:width) { 800 }
+      let(:height) { 300 }
+
+      it "crops the image to the maximum aspect ratio" do
+        generator.generate_image(original_path)
+
+        expect(image).to have_received(:resize).with("800x419^")
+        expect(image).to have_received(:gravity).with("center")
+        expect(image).to have_received(:extent).with("800x419")
+      end
+    end
+
+    context "when the image is too tall" do
+      let(:width) { 800 }
+      let(:height) { 1200 }
+
+      it "crops the image to the minimum aspect ratio" do
+        generator.generate_image(original_path)
+
+        expect(image).to have_received(:resize).with("800x1066^")
+        expect(image).to have_received(:gravity).with("center")
+        expect(image).to have_received(:extent).with("800x1066")
+      end
+    end
+
+    it "writes and returns a uniquely named image" do
+      result = generator.generate_image(original_path)
+
+      expect(image).to have_received(:write).with(caption_path)
+      expect(result).to eq(caption_path)
+    end
+  end
 end
