8029a209c1312b504d1b1198320732a1b3fc24a4
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2026-07-25 15:08:10 +0300

    list instagram captions

--- app/controllers/instagram_captions_controller.rb
+++ app/controllers/instagram_captions_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class InstagramCaptionsController < ApplicationController
+  def index
+    captions = InstagramCaption.order(:id)
+
+    render json: {
+      captions: captions.as_json(
+        only: %i[id url type text filter caption_url]
+      )
+    }, status: :ok
+  end
 end
